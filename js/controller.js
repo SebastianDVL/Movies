0d5263635cb820ca99d8e3382ce77cbcd6c99353
Sebas <sebas.cay@gmail.com>
2022-04-16 20:43:42 -0500

    pw-003 Se agrega seccion de mejores votados y peliculas proximamente en aside

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,22 @@
 import {useAPI,APIKEY} from './consumirAPI.js';
-import {showPopularMovies} from './deployInfo.js';
+import {showMovies} from './deployInfo.js';
+import {showUpcomingMovies} from './deployInfo.js';
 
 
+//se declaran todas las uris 
 let uriPopulares = `https://api.themoviedb.org/3/movie/popular?api_key=${APIKEY}&language=en-US&page=1`
-let popularMovies = await useAPI(uriPopulares);
-showPopularMovies(popularMovies)
+let uriUpcoming = `https://api.themoviedb.org/3/movie/upcoming?api_key=${APIKEY}&language=en-US&page=1`
+let uriRated = `https://api.themoviedb.org/3/movie/top_rated?api_key=${APIKEY}&language=en-US&page=1`
 
-console.log(popularMovies)
+//1
+let popularMovies = await useAPI(uriPopulares);
+const contenedorPopulares = document.querySelector('#populares')
+showMovies(popularMovies,contenedorPopulares)
+//2
+let upcomingMovies = await useAPI(uriUpcoming);
+showUpcomingMovies(upcomingMovies);
+//3
+let ratedMovies = await useAPI(uriRated)
+const contenedorRated = document.querySelector('#rated')
+showMovies(ratedMovies,contenedorRated);
+console.log(ratedMovies)
--- js/deployInfo.js
+++ js/deployInfo.js
@@ -1,15 +1,34 @@
-export function showPopularMovies(movies){
+export function showMovies(movies,contenedor){
 
     const popularMovieTemplate = document.querySelector('[data-populares]')
-    const contenedorPopulares = document.querySelector('#populares')
+
     movies.results.forEach((result,index) => {
         let container = popularMovieTemplate.content.cloneNode(true).children[0]
         container.querySelector('img').src = `https://image.tmdb.org/t/p/w500${result.poster_path}`
-        container.querySelector('p').textContent = result.original_title
-        container.querySelector('h5').textContent = result.release_date
+        container.querySelector('p').textContent = result.title
+        container.querySelector('h5').textContent = result.release_date.substring(0, 4)
+        let size = result.vote_average.toFixed(1).replace('.',"")
+        container.querySelector('h6').textContent = size + "%"
+        container.querySelector('input').style.backgroundSize = `${size}% 100%`
         if(index % 2 == 0){
             container.classList.add('snap-start')
         }
-        contenedorPopulares.appendChild(container)
+
+        contenedor.appendChild(container)
     })
 }
+
+
+export function showUpcomingMovies(movies){
+    const contenedorUpcoming = document.querySelector('#Estrenos')
+
+    movies.results.forEach(result =>{
+        let img = document.createElement('img')
+        img.src = `https://image.tmdb.org/t/p/w500${result.poster_path}`
+        img.classList.add("aspect-square")
+
+        contenedorUpcoming.appendChild(img)
+    })
+     
+}
+
